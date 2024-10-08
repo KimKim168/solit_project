0014ae50639a142242e944053a75e0fa0e31bd67
Kimsoreya168 <kimsoreya093@gmial.com>
2024-10-08 13:09:35 +0700

    up

--- resources/views/client/latest_news.blade.php
+++ resources/views/client/latest_news.blade.php
@@ -33,13 +33,14 @@
                                 <strong
                                     class="bg-gradient-to-r font-bold from-[#2d37a4] to-teal-500 text-transparent bg-clip-text md:text-xl ">Custom
                                     Mobile App Development</strong>
-                                <p> We specialize in building bespoke mobile applications tailored to your specific business
+                                <p class="text-sm md:text-[16px]"> We specialize in building bespoke mobile applications
+                                    tailored to your specific business
                                     needs. Whether you’re looking for a customer-facing app or a robust internal tool, our
                                     team will work with you from concept to launch to ensure your app meets your objectives.
                                     We offer:
                                 </p>
                                 {{-- Item1 --}}
-                                <ul class="list-disc px-4 space-y-2">
+                                <ul class="list-disc px-4 space-y-2 text-sm md:text-[16px]">
                                     <li>
                                         Native app development for iOS and Android platforms
                                     </li>
@@ -56,13 +57,14 @@
                                 <strong
                                     class="bg-gradient-to-r font-bold from-[#2d37a4] to-teal-500 text-transparent bg-clip-text md:text-xl ">iOS
                                     App Development</strong>
-                                <p> Our team of expert iOS developers is proficient in building high-performance, scalable,
+                                <p class="text-sm md:text-[16px]"> Our team of expert iOS developers is proficient in
+                                    building high-performance, scalable,
                                     and secure apps for the Apple ecosystem. We ensure that your app is optimized for the
                                     latest iOS devices and meets all App Store guidelines, providing seamless performance on
                                     iPhones, iPads, and other Apple devices. Key features include:
                                 </p>
                                 {{-- Item2 --}}
-                                <ul class="list-disc px-4 space-y-2">
+                                <ul class="list-disc px-4 space-y-2 text-sm md:text-[16px] ">
                                     <li>
                                         Swift and Objective-C programming
                                     </li>
@@ -79,13 +81,14 @@
                                 <strong
                                     class="bg-gradient-to-r font-bold from-[#2d37a4] to-teal-500 text-transparent bg-clip-text md:text-xl ">Android
                                     App Development</strong>
-                                <p>Our Android app development team creates apps that are fully compatible with the Android
+                                <p class="text-sm md:text-[16px]">Our Android app development team creates apps that are
+                                    fully compatible with the Android
                                     ecosystem, including smartphones, tablets, wearables, and more. We deliver Android apps
                                     that provide a consistent and engaging user experience across all devices. Our Android
                                     app development services include:
                                 </p>
                                 {{-- Item3 --}}
-                                <ul class="list-disc px-4 space-y-2">
+                                <ul class="list-disc px-4 space-y-2 text-sm md:text-[16px]">
                                     <li>
                                         Java and Kotlin programming
                                     </li>
@@ -102,13 +105,14 @@
                                 <strong
                                     class="bg-gradient-to-r font-bold from-[#2d37a4] to-teal-500 text-transparent bg-clip-text md:text-xl ">Cross-Platform
                                     App Development</strong>
-                                <p>To help you reach the widest possible audience, we offer cross-platform app development
+                                <p class="text-sm md:text-[16px]">To help you reach the widest possible audience, we offer
+                                    cross-platform app development
                                     services that allow your app to run on both iOS and Android using a single codebase.
                                     This approach reduces development time and costs while ensuring your app functions
                                     flawlessly across multiple platforms. We utilize popular frameworks such as:
                                 </p>
                                 {{-- Item4 --}}
-                                <ul class="list-disc px-4 space-y-2">
+                                <ul class="list-disc px-4 space-y-2 text-sm md:text-[16px]">
                                     <li>
                                         React Native
                                     </li>
@@ -125,12 +129,13 @@
                                 <strong
                                     class="bg-gradient-to-r font-bold from-[#2d37a4] to-teal-500 text-transparent bg-clip-text md:text-xl ">UI/UX
                                     Design</strong>
-                                <p>A great mobile app isn’t just about functionality; it’s also about delivering an
+                                <p class="text-sm md:text-[16px]">A great mobile app isn’t just about functionality; it’s
+                                    also about delivering an
                                     exceptional user experience. Our UI/UX design team focuses on creating visually stunning
                                     interfaces that are easy to navigate and provide a seamless experience. We focus on:
                                 </p>
                                 {{-- Item5 --}}
-                                <ul class="list-disc px-4 space-y-2">
+                                <ul class="list-disc px-4 space-y-2 text-sm md:text-[16px]">
                                     <li>
                                         Intuitive and user-friendly navigation
                                     </li>
@@ -150,11 +155,12 @@
                                 <strong
                                     class="bg-gradient-to-r font-bold from-[#2d37a4] to-teal-500 text-transparent bg-clip-text md:text-xl ">Mobile
                                     App Maintenance and Support</strong>
-                                <p>Our services don’t stop after launch. We offer ongoing maintenance and support to ensure
+                                <p class="text-sm md:text-[16px]">Our services don’t stop after launch. We offer ongoing
+                                    maintenance and support to ensure
                                     your mobile app continues to perform optimally. This includes:
                                 </p>
                                 {{-- Item6 --}}
-                                <ul class="list-disc px-4 space-y-2">
+                                <ul class="list-disc px-4 space-y-2 text-sm md:text-[16px]">
                                     <li>
                                         Regular updates to ensure compatibility with new operating system versions
                                     </li>
@@ -175,12 +181,13 @@
                                 <strong
                                     class="bg-gradient-to-r font-bold from-[#2d37a4] to-teal-500 text-transparent bg-clip-text md:text-xl ">Enterprise
                                     Mobile Solutions</strong>
-                                <p>We build enterprise-level mobile applications that streamline operations, improve
+                                <p class="text-sm md:text-[16px]">We build enterprise-level mobile applications that
+                                    streamline operations, improve
                                     workflow, and boost productivity within your organization. Our enterprise solutions are
                                     customized to meet the specific needs of your business and may include:
                                 </p>
                                 {{-- Item7 --}}
-                                <ul class="list-disc px-4 space-y-2">
+                                <ul class="list-disc px-4 space-y-2 text-sm md:text-[16px]">
                                     <li>
                                         Employee management systems
                                     </li>
@@ -201,7 +208,7 @@
                                 <strong
                                     class="bg-gradient-to-r font-bold from-[#2d37a4] to-teal-500 text-transparent bg-clip-text md:text-xl ">API
                                     and Third-Party Integrations</strong>
-                                <p>
+                                <p class="text-sm md:text-[16px]">
                                     We integrate third-party services and APIs into your app to extend functionality and
                                     provide additional features. Whether you need to integrate payment gateways, social
                                     media platforms, location services, or cloud-based solutions, we ensure seamless
@@ -213,12 +220,13 @@
                                 <strong
                                     class="bg-gradient-to-r font-bold from-[#2d37a4] to-teal-500 text-transparent bg-clip-text md:text-xl ">E-Commerce
                                     and mCommerce App Development</strong>
-                                <p>For businesses looking to sell products or services online, we offer eCommerce and mobile
+                                <p class="text-sm md:text-[16px]">For businesses looking to sell products or services
+                                    online, we offer eCommerce and mobile
                                     commerce app development. We create robust, secure, and user-friendly eCommerce apps
                                     that enable customers to browse, shop, and make purchases easily. Features include:
                                 </p>
                                 {{-- Item9 --}}
-                                <ul class="list-disc px-4 space-y-2">
+                                <ul class="list-disc px-4 space-y-2 text-sm md:text-[16px]">
                                     <li>
                                         Custom shopping cart development
                                     </li>
@@ -238,12 +246,13 @@
                                 <strong
                                     class="bg-gradient-to-r font-bold from-[#2d37a4] to-teal-500 text-transparent bg-clip-text md:text-xl ">App
                                     Testing and Quality Assurance</strong>
-                                <p>Our mobile app testing services ensure that your app is free of bugs and performs well
+                                <p class="text-sm md:text-[16px]">Our mobile app testing services ensure that your app is
+                                    free of bugs and performs well
                                     across all devices and operating systems. We conduct thorough testing to ensure the
                                     highest quality, focusing on:
                                 </p>
                                 {{-- Item10 --}}
-                                <ul class="list-disc px-4 space-y-2">
+                                <ul class="list-disc px-4 space-y-2 text-sm md:text-[16px]">
                                     <li>
                                         Functional testing to ensure all features work as expected
                                     </li>
@@ -271,7 +280,8 @@
                                 <strong
                                     class="bg-gradient-to-r font-bold from-[#2d37a4] to-teal-500 text-transparent bg-clip-text md:text-xl ">Discovery
                                     & Requirement Gathering:</strong>
-                                <p>We begin by understanding your business goals and mobile app requirements. This phase
+                                <p class="text-sm md:text-[16px]">We begin by understanding your business goals and mobile
+                                    app requirements. This phase
                                     includes market research, competitor analysis, and identifying the key features and
                                     functionality of your app.
                                 </p>
@@ -281,7 +291,8 @@
                                 <strong
                                     class="bg-gradient-to-r font-bold from-[#2d37a4] to-teal-500 text-transparent bg-clip-text md:text-xl ">Design
                                     & Prototyping:</strong>
-                                <p>Our design team creates wireframes and interactive prototypes to give you a clear vision
+                                <p class="text-sm md:text-[16px]">Our design team creates wireframes and interactive
+                                    prototypes to give you a clear vision
                                     of the app’s layout and user flow. We ensure that the design aligns with your brand and
                                     provides a user-friendly experience.
                                 </p>
@@ -292,7 +303,8 @@
                                 <strong
                                     class="bg-gradient-to-r font-bold from-[#2d37a4] to-teal-500 text-transparent bg-clip-text md:text-xl ">Development
                                     & Testing:</strong>
-                                <p>Once the design is approved, our developers begin coding your app. We follow Agile
+                                <p class="text-sm md:text-[16px]">Once the design is approved, our developers begin coding
+                                    your app. We follow Agile
                                     development methodologies to ensure timely delivery and flexibility for updates. During
                                     development, we conduct regular testing to identify and resolve any issues.
                                 </p>
@@ -302,7 +314,8 @@
                                 <strong
                                     class="bg-gradient-to-r font-bold from-[#2d37a4] to-teal-500 text-transparent bg-clip-text md:text-xl ">Launch
                                     & Deployment:</strong>
-                                <p>After thorough testing, we prepare your app for launch. We handle the submission process
+                                <p class="text-sm md:text-[16px]">After thorough testing, we prepare your app for launch. We
+                                    handle the submission process
                                     to the Apple App Store and Google Play Store, ensuring that your app meets all necessary
                                     guidelines for approval.
                                 </p>
@@ -312,7 +325,8 @@
                                 <strong
                                     class="bg-gradient-to-r font-bold from-[#2d37a4] to-teal-500 text-transparent bg-clip-text md:text-xl ">Ongoing
                                     Support & Maintenance:</strong>
-                                <p>Post-launch, we continue to monitor the app’s performance, making improvements based on
+                                <p class="text-sm md:text-[16px]">Post-launch, we continue to monitor the app’s performance,
+                                    making improvements based on
                                     user feedback, ensuring compatibility with new OS updates, and providing any necessary
                                     maintenance and support.
                                 </p>
